f343702a2f4cf9a31bb8054d79b2ac21c7ade927
Selorm Sosuh <selsosuh@gmail.com>
2024-06-14 13:41:10 -0500

    file creation addition

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod cipher;
 use cipher::translate;
+use std::fs::File;
+use std::io::prelude::*;
 use std::{fs, io};
 
 fn main() {
-    //Turns contents of text file into a tring, return "Should've" if the process fails
-    let contents = fs::read_to_string("XenobladeScriptE.txt").expect("Shoudlve");
+    //Turns contents of text file into a string, return "Should've" if the process fails
+    let contents = fs::read_to_string("Xenoblade2.txt").expect("Shoudlve");
 
     println!("encrypt or decrypt?");
     let mut mode = String::new();
@@ -14,5 +16,7 @@ fn main() {
     io::stdin().read_line(&mut key).unwrap();
     let num: isize = key.trim().parse().unwrap();
     let result = translate(contents, mode, num);
-    println!("{}", result);
+    let mut file = File::create("Xenoblade2.txt").expect("nah id win");
+    file.write(result.as_bytes()).expect("gaming");
+    println!("Encrypted/Decrypted File Created");
 }
